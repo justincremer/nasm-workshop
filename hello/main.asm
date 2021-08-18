2ea796d88d7c4068c8ef34d172407b051f049c19
justincremer <jacremer@live.com>
2021-08-17 21:30:05 -0700

    64 bit mode configuration

--- hello/main.asm
+++ hello/main.asm
@@ -1,20 +1,30 @@
-
 section	.data
-msg db 'Hello, world!', 0xa  ;string to be printed
-len equ $ - msg     ;length of the string
+	msg db 'hello world', 0xa  ; string to be printed
+	len equ $ - msg     ; length of the string
 
 section	.bss
 	
 section	.text
-   global _start     ;must be declared for linker (ld)
-	
-_start:	            ;tells linker entry point
-   mov	edx,len     ;message length
-   mov	ecx,msg     ;message to write
-   mov	ebx,1       ;file descriptor (stdout)
-   mov	eax,4       ;system call number (sys_write)
-   int	0x80        ;call kernel
-	
-   mov	eax,1       ;system call number (sys_exit)
-	int	0x80        ;call kernel
+global _start     ; must be declared for linker (ld)
+
+; 32 bit mode	
+_start:	            ; tells linker entry point
+	mov	edx,len     ; message length
+	mov	ecx,msg     ; message to write
+	mov	ebx,1       ; file descriptor (stdout)
+	mov	eax,4       ; system call number (sys_write)
+	int	0x80        ; call kernel
+
+	mov	eax,1       ; system call number (sys_exit)
+	int	0x80        ; call kernel
+
+; _start:	            ; tells linker entry point
+; 	mov	rdx,len     ; message length
+; 	mov	rcx,msg     ; message to write
+; 	mov	rbx,1       ; file descriptor (stdout)
+; 	mov	rax,4       ; system call number (sys_write)
+; 	int	0x80        ; call kernel
+
+; 	mov	rax,1       ; system call number (sys_exit)
+; 	int	0x80        ; call kernel
 
