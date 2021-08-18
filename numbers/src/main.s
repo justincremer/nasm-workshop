8c4b5de0e4faa34b0de90ec8069b010c73c3e68a
justincremer <jacremer@live.com>
2021-08-18 13:18:41 -0700

    breaks numbers out into procs and macros

--- numbers/src/main.s
+++ numbers/src/main.s
@@ -1,35 +1,40 @@
+%macro print 2
+	mov ecx, %1 
+	mov edx, %2 
+	mov ebx, 1	         ; file descriptor (stdout)
+	mov eax, 4	         ; system call number (sys_write)
+	int 0x80	         ; call kernel	
+%endmacro
+	
+section	.text
+global _start
+
+_start:
+	call math
+	print msg, msg_len
+	print sum, 1
+	print nl, nl_len
+	call exit
+
+math:
+	mov eax, '3'
+	mov ebx, '4'
+	add eax, ebx
+	sub eax, '3'
+	mov [sum], eax		 ; stores result in [sum]
+	ret
+
+exit:
+	mov eax, 1	         ; system call number (sys_exit)
+	int 0x80	         ; call kernel
+	ret
+	
 section	.data
-	msg db "The sum is:", 0xA,0xD
-	len equ $ - msg
+	msg db "The sum is: "
+	msg_len equ $ - msg
+	nl db 0xA, 0xD
+	nl_len equ $ - nl
 
 section	.bss
 	sum resb 1
-	
-section	.text
-global _start 
-
-_start:
-	mov eax, '3'
-	sub eax, '0'
-
-	mov ebx, '4'
-	sub ebx, '0'
-	add eax, ebx
-	add eax, '0'
-
-	mov [sum], eax
-	mov ecx, msg	
-	mov edx, len
-	mov ebx, 1	         ; file descriptor (stdout)
-	mov eax, 4	         ; system call number (sys_write)
-	int 0x80	         ; call kernel
-
-	mov ecx, sum
-	mov edx, 1
-	mov ebx, 1	         ; file descriptor (stdout)
-	mov eax, 4	         ; system call number (sys_write)
-	int 0x80	         ; call kernel
-
-	mov eax, 1	         ; system call number (sys_exit)
-	int 0x80	         ; call kernel
 
