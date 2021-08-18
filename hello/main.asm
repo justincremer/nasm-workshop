d8667506e848640bfd7de729628db0da458d10ae
justincremer <jacremer@live.com>
2021-08-17 21:14:46 -0700

    generalizes makefile

--- hello/main.asm
+++ hello/main.asm
@@ -1,3 +1,10 @@
+
+section	.data
+msg db 'Hello, world!', 0xa  ;string to be printed
+len equ $ - msg     ;length of the string
+
+section	.bss
+	
 section	.text
    global _start     ;must be declared for linker (ld)
 	
@@ -9,8 +16,5 @@ _start:	            ;tells linker entry point
    int	0x80        ;call kernel
 	
    mov	eax,1       ;system call number (sys_exit)
-   int	0x80        ;call kernel
+	int	0x80        ;call kernel
 
-section	.data
-msg db 'Hello, world!', 0xa  ;string to be printed
-len equ $ - msg     ;length of the string
